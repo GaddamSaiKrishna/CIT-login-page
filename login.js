b631c8d7dc18625de1dbf43816933e0dfa07900d
GaddamSaiKrishna <gaddamsaikrishna460@gmail.com>
2024-02-25 21:52:05 +0530

    upd:changes

--- login.js
+++ login.js
@@ -25,7 +25,7 @@ fetch("https://www.googleapis.com/oauth2/v3/userinfo",{
 .then((data)=>data.json())
 .then((info)=>{
     console.log(info);
-    var nameValue='info.name';
+    var nameValue=info.name;
     console.log(nameValue);
    document.querySelector("#name").value=nameValue;
 })
