7ff0b4e1e288a792f2050981b582b9a34264ad79
GaddamSaiKrishna <gaddamsaikrishna460@gmail.com>
2024-02-26 15:27:49 +0530

    upd:modify

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
-        let btn1=document.querySelector("#about");
-        let btn2=document.querySelector("#login");
-        btn1.addEventListener('mousemove',e=>{
-            let rect=e.target.getBoundingClientRect();
-            let x=e.clientX*3-rect.left;
-            btn1.style.setProperty('--x',x + 'deg');
-        })
-        btn2.addEventListener('mousemove',e=>{
-            let rect=e.target.getBoundingClientRect();
-            let x=e.clientX-rect.left;
-            btn2.style.setProperty('--x',x + 'deg');
-        })
+let btn1=document.querySelector("#about");
+let btn2=document.querySelector("#login");
+btn1.addEventListener('mousemove',e=>{
+    let rect=e.target.getBoundingClientRect();
+    let x=e.clientX*3-rect.left;
+    btn1.style.setProperty('--x',x + 'deg');
+})
+btn2.addEventListener('mousemove',e=>{
+    let rect=e.target.getBoundingClientRect();
+    let x=e.clientX-rect.left;
+    btn2.style.setProperty('--x',x + 'deg');
+})
+
 function signIn(){
     let oauth2Endpoint="https://accounts.google.com/o/oauth2/v2/auth";
     let form =document.createElement('form');
@@ -18,7 +19,7 @@ function signIn(){
     let params ={
         "client_id":"581333481597-8rrrin5jg3kcaum95m572qrvk02gjp9p.apps.googleusercontent.com",
         "redirect_uri":"https://gaddamsaikrishna.github.io/CIT-login-page/login.html",
-        "response_type":"token", //token or code
+        "response_type":"token",
         "scope":"https://www.googleapis.com/auth/userinfo.profile",
         "include_granted_scopes":'true',
         'state':'pass-through-value'
