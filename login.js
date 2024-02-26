ce50c7dbbc1a2d3f8ac1af9c06dfc53f4b7c8610
GaddamSaiKrishna <gaddamsaikrishna460@gmail.com>
2024-02-26 17:43:44 +0530

    add:firebase

--- login.js
+++ login.js
@@ -30,31 +30,4 @@ fetch("https://www.googleapis.com/oauth2/v3/userinfo",{
    document.querySelector("#name").value=nameValue;
 })
 
-const firebaseConfig = {
-    apiKey: "AIzaSyCLgBdTdf2clX4LZTHyGBZMQuAZSJtIoQc",
-    authDomain: "loginpagecit.firebaseapp.com",
-    databaseURL: "https://loginpagecit-default-rtdb.firebaseio.com",
-    projectId: "loginpagecit",
-    storageBucket: "loginpagecit.appspot.com",
-    messagingSenderId: "364150150556",
-    appId: "1:364150150556:web:d2e6e8fc7533c050a32459"
-  };
- // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-//
-var loginFormDB=firebase.database().ref('loginForm')
-document.querySelector("#submit").addEventListener("click",submitForm);
-function submitForm(e){
-    e.preventDefault();
-    var name=getElementVal(name);
-    var username=getElementVal(username);
-    var number=getElementVal(number);
-    var college=getElementVal(college);
-    var branch=getElementVal(branch);
 
-    console.log(name,username,number,college,branch)
-}
-
-const getElementVal = (id)=>{
-    return document.getElementById(id).value;
-}
