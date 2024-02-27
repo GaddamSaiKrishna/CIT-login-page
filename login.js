d82dd529db58474ec020a886aefba368a44b1878
GaddamSaiKrishna <gaddamsaikrishna460@gmail.com>
2024-02-27 23:22:44 +0530

    modify

--- login.js
+++ login.js
@@ -29,5 +29,31 @@ fetch("https://www.googleapis.com/oauth2/v3/userinfo",{
     console.log(nameValue);
    document.querySelector("#name").value=nameValue;
 })
+let reg = document.querySelector(".reg");
+let wel = document.querySelector(".wel");
 
+document.getElementById("loginForm").addEventListener("submit", function(e) {
+    e.preventDefault();
+    reg.classList.add("hidden");
+    wel.classList.remove("hidden");
+
+    // Get form values
+    let name = document.getElementById("name").value;
+    let username = document.getElementById("username").value;
+    let number = document.getElementById("number").value;
+    let college = document.getElementById("college").value;
+    let branch = document.getElementById("branch").value;
+    let year = document.getElementById("year").value;
+
+    // Display form values
+    let welContent = document.querySelector(".wel ul");
+    welContent.innerHTML = `
+        <li>Name: ${name}</li>
+        <li>Username: ${username}</li>
+        <li>Mobile No.: ${number}</li>
+        <li>College/School: ${college}</li>
+        <li>Department: ${branch}</li>
+        <li>Year of Study: ${year}</li>
+    `;
+});
 
